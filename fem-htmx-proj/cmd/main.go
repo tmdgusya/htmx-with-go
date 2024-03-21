2bd653b92ea6e84f2b48acbc0e49f79224e2d270
정승현 <tmdgusya@naver.com>
2024-03-21 21:50:23 +0900

    with counting block

--- fem-htmx-proj/cmd/main.go
+++ fem-htmx-proj/cmd/main.go
@@ -31,15 +31,12 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Renderer = newTemplate()
 	count := Count{Count: 0}
-
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(200, "index", count)
 	})
-
 	e.POST("/count", func(c echo.Context) error {
 		count.Count++
-		return c.Render(200, "index", count)
+		return c.Render(200, "count", count)
 	})
-
 	e.Logger.Fatal(e.Start(":42069"))
 }
